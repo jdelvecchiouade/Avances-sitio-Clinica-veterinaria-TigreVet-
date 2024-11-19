69cfc1f95ab54853df165c15512c34a37371f5f5
matpurita <matpurita@uade.edu.ar>
2024-11-19 15:29:13 -0300

    agrego logica boton scroll to top

--- inicio.js
+++ inicio.js
@@ -8,4 +8,19 @@ $(document).ready(function() {
         $('#menu').removeClass('show');
       }
     });
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 30) {
+          $('#scrollToTopBtn').fadeIn();
+        } else {
+          $('#scrollToTopBtn').fadeOut();
+        }
+      });
+    
+      // Subir hasta arriba cuando se hace clic en el botón
+      $('#scrollToTopBtn').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, '300');
+        return false;
+      });
+    
   });
